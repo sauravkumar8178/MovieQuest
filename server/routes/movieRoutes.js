97937c01c377eb71293997bd003dcb65517aaf63
Saurav Kumar <ss989108@gmail.com>
2024-08-09 13:33:33 +0530

    implement only react

--- server/routes/movieRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { fetchMovieData, getMovies } from '../controllers/movieController';
-
-const router = express.router();
-
-router.get('/fetch', fetchMovieData);
-router.get('/', getMovies);
-
-export default router;
